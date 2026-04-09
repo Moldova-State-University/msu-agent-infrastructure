e0d470acc9cd19600cb9e09380de0ddb3886cb61
/rdndev <rdninc@mail.ru>
2026-04-09 15:36:37 +0300

    feat(infra-9): migrate terraform to terraform cloud

--- github/versions.tf
+++ github/versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
+
+  cloud {
+    organization = "msu-agent"
+
+    workspaces {
+      name = "msu-agent-github-infra"
+    }
+  }
+
   required_providers {
     github = {
       source  = "integrations/github"
